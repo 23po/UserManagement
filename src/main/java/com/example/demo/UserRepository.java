cebc931fcac099f61f7abda296dc4ca387a4c38e
23po <ckodalo@gmail.com>
2023-10-10 05:11:08 +0300

    added userRepository

---
+++ src/main/java/com/example/demo/UserRepository.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import com.example.demo.*;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User save(User user);
+
+    Optional<User> findById(Long id);
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+    List<User> findAll();
+
+    void delete(User user);
+    
+}
