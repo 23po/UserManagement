48174abac576444bce8aafe85961493c9af00bca
23po <ckodalo@gmail.com>
2023-11-11 00:20:03 +0300

    edit

--- src/main/java/com/example/demo/Transaction/Transaction.java
+++ src/main/java/com/example/demo/Transaction/Transaction.java
@@ -1,14 +1,17 @@
 package com.example.demo.Transaction;
 
+import lombok.Data;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@Data
 public class Transaction {
 
     private Long transactionId;
     private Long senderUserId;
     private Long recipientUserId;
 
-    private BigDecimal transactionAmount;
-    private LocalDateTime transactionDateTime;
+    private BigDecimal amount;
+    private LocalDateTime timestamp;
 }
