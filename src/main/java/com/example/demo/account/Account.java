b1e8835b523ecaabee7cbc61d59a4f5919118065
23po <ckodalo@gmail.com>
2023-11-13 03:14:04 +0300

    adds username field

--- src/main/java/com/example/demo/account/Account.java
+++ src/main/java/com/example/demo/account/Account.java
@@ -1,18 +1,26 @@
 package com.example.demo.account;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
+@Entity
 public class Account {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long accountId;
     Long userId;
+    String username;
     BigDecimal balance;
     String currency;
 
-    public Account(Long accountId, Long userId, BigDecimal balance, String currency) {
+    public Account(Long accountId, Long userId, String username, BigDecimal balance, String currency) {
 
         this.userId = userId;
         this.accountId = accountId;
