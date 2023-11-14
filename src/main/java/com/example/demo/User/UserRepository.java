ef233f81dab64bceb4e6758a72d53b8d1d894477
23po <ckodalo@gmail.com>
2023-11-14 05:00:02 +0300

    edits

--- src/main/java/com/example/demo/User/UserRepository.java
+++ src/main/java/com/example/demo/User/UserRepository.java
@@ -7,16 +7,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    //User save(User user);
-    Optional<User> findById(Long id);
-
     Optional<User> findByUsername(String username);
 
     Optional<User> findByEmail(String email);
 
-
-    List<User> findAll();
-
-    void delete(User user);
     
 }
