f7dd01c2c5cdf7b7f1f68fb0af378a81d9c1bba8
23po <ckodalo@gmail.com>
2023-11-15 17:57:56 +0300

    EDIT: accounts created on user registration

--- src/main/java/com/example/demo/auth/AuthenticationService.java
+++ src/main/java/com/example/demo/auth/AuthenticationService.java
@@ -1,5 +1,7 @@
 package com.example.demo.auth;
 
+import java.math.BigDecimal;
+
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -8,6 +10,9 @@ import org.springframework.stereotype.Service;
 import com.example.demo.User.Role;
 import com.example.demo.User.User;
 import com.example.demo.User.UserRepository;
+import com.example.demo.account.Account;
+import com.example.demo.account.AccountRepository;
+import com.example.demo.account.AccountService;
 import com.example.demo.config.JwtService;
 
 import lombok.RequiredArgsConstructor;
@@ -18,6 +23,10 @@ public class AuthenticationService {
     
     private final UserRepository repository;
 
+    private final AccountRepository accountRepository;
+
+    //private final AccountService accountService;
+
     private final PasswordEncoder passwordEncoder;
 
     private final AuthenticationManager authenticationManager;
@@ -25,6 +34,8 @@ public class AuthenticationService {
     private final JwtService jwtService;
 
     public AuthenticationResponse register(RegisterRequest request) {
+        var account = new Account();
+        
         var user = User.builder()
             .username(request.getUsername())
             .email(request.getEmail())
@@ -32,13 +43,28 @@ public class AuthenticationService {
             //.role(request.getRole())
             .role(Role.USER)
             .build();
+       
+
+        
+        account.setBalance(BigDecimal.ZERO);
+        account.setUser(user);
+        account.setUsername(user.getUsername());
+        
+
+    
+        user.setAccount(account);
         repository.save(user);
+
+        //accountRepository.save(account);
+
+
         var jwtToken = jwtService.generateToken(user);
 
         return AuthenticationResponse.builder()
             .token(jwtToken)
             .build();
     }
+
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
         authenticationManager.authenticate(
