220d598b4c0406bcf4c379b2f3b8f2e87b1329d9
23po <ckodalo@gmail.com>
2023-11-28 19:01:38 +0300

    base64 encoder

--- src/main/java/com/example/demo/mpesa/Constants.java
+++ src/main/java/com/example/demo/mpesa/Constants.java
@@ -1,9 +1,9 @@
-package com.example.demo;
+package com.example.demo.mpesa;
 
 public class Constants {
 
     public static final String BASIC_AUTH_STRING = "Basic";
-    public static final String AUTHORIZATION_HEADER_STRING = "authorization";
+    public static final String AUTHORIZATION_HEADER_STRING = "Authorization";
     public static final String CACHE_CONTROL_HEADER = "cache-control";
     public static final String CACHE_CONTROL_HEADER_VALUE = "no-cache";
 
---
+++ src/main/java/com/example/demo/mpesa/HelperUtility.java
@@ -0,0 +1,20 @@
+package com.example.demo.mpesa;
+
+import lombok.extern.slf4j.Slf4j;
+//import org.bson.internal.Base64;
+import java.util.Base64;
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
+public class HelperUtility {
+
+    /**
+     * @param value the value to be converted to a base64 string
+     * @return returns base64String
+     */
+    public static String toBase64String(String value) {
+        byte[] data = value.getBytes(StandardCharsets.ISO_8859_1);
+        return Base64.getEncoder().encodeToString(data);
+    }
+
+}
