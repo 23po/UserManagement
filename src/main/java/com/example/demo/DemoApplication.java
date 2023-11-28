c6660e4956c67d406a05f647b97d3a160ad3e900
23po <ckodalo@gmail.com>
2023-11-28 19:01:16 +0300

    inject okhhtp objectmapper beans to spring

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import okhttp3.OkHttpClient;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,5 +14,15 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	@Bean
+    public OkHttpClient getOkHttpClient() {
+        return new OkHttpClient();
+    }
+
+    @Bean
+    public ObjectMapper getObjectMapper() {
+        return new ObjectMapper();
+    }
 
 }
