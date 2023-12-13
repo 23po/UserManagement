5c18e719db93094b6e5f56161a644606d4cf6b0a
23po <ckodalo@gmail.com>
2023-12-13 23:00:29 +0300

    adds b2c config params

--- src/main/java/com/example/demo/config/MpesaConfig.java
+++ src/main/java/com/example/demo/config/MpesaConfig.java
@@ -13,6 +13,17 @@ public class MpesaConfig {
     private String consumerSecret;
     private String grantType;
     private String oauthEndpoint;
+    private String registerUrlEndpoint;
+    private String simulateTransactionEndpoint;
+    private String shortCode;
+    private String confirmationURL;
+    private String validationURL;
+    private String responseType;
+    private String b2cTransactionEndpoint;
+    private String b2cResultUrl;
+    private String b2cQueueTimeoutUrl;
+    private String b2cInitiatorName;
+    private String b2cInitiatorPassword;
 
     @Override
     public String toString() {
