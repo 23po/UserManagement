43bc214f4afcef5c39727d7bac8cf066a3ba5f98
23po <ckodalo@gmail.com>
2023-12-13 22:58:20 +0300

    adds bean

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.demo.mpesa.dto.AcknowledgeResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -23,6 +24,13 @@ public class DemoApplication {
     @Bean
     public ObjectMapper getObjectMapper() {
         return new ObjectMapper();
+    }
+
+    @Bean
+    public AcknowledgeResponse getAcknowledgeResponse() {
+        AcknowledgeResponse acknowledgeResponse = new AcknowledgeResponse();
+        acknowledgeResponse.setMessage("Success");
+        return acknowledgeResponse;
     }
 
 }
